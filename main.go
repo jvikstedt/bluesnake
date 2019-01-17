449f8fb55ab7b0c289f1fbbb106b21eb2e558e92
Janne Vikstedt <vikstedt.janne@gmail.com>
2019-01-17 20:57:39 +0200

    Testing out agent metadata

--- main.go
+++ main.go
@@ -10,18 +10,37 @@ import (
 	"github.com/jvikstedt/bluestorm/network/json"
 )
 
-func onConnect(a *network.Agent) {
-	log.Printf("Agent connectedted %s\n", a.ID())
+func onConnect(agent *network.Agent) {
+	log.Printf("Agent connectedted %s\n", agent.ID())
 	defaultRoom, _ := hub.DefaultManager().GetRoom(hub.DefaultRoomID)
-	hub.DefaultManager().UserToRoom(hub.UserID(a.ID()), hub.DefaultRoomID)
+	err := hub.DefaultManager().UserToRoom(hub.UserID(agent.ID()), hub.DefaultRoomID)
+	if err != nil {
+		log.Println(err)
+		agent.Conn().Close()
+		return
+	}
+	agent.SetValue("room", defaultRoom)
 
 	log.Printf("room has users: %v\n", defaultRoom.GetUsers())
 }
 
-func onDisconnect(a *network.Agent) {
-	log.Printf("Agent disconnected %s\n", a.ID())
+func onDisconnect(agent *network.Agent) {
+	log.Printf("Agent disconnected %s\n", agent.ID())
 	defaultRoom, _ := hub.DefaultManager().GetRoom(hub.DefaultRoomID)
-	hub.DefaultManager().RemoveUser(hub.UserID(a.ID()))
+	err := hub.DefaultManager().RemoveUser(hub.UserID(agent.ID()))
+	if err != nil {
+		log.Println(err)
+	}
+
+	r, err := agent.GetValue("room")
+	if err != nil {
+		log.Println(err)
+	}
+	room, ok := r.(*hub.Room)
+	if !ok {
+		log.Println("not okay...")
+	}
+	log.Printf("Was in room %s\n", room.ID())
 	log.Printf("room has users: %v\n", defaultRoom.GetUsers())
 }
 
