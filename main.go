6216e81af3601ef749aef1562191ecbb6191cf6a
Janne Vikstedt <vikstedt.janne@gmail.com>
2019-01-17 20:28:06 +0200

    Testing new hub feature of bluestorm engine

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 	"os"
 
 	"github.com/jvikstedt/bluestorm"
+	"github.com/jvikstedt/bluestorm/hub"
 	"github.com/jvikstedt/bluestorm/network"
 	"github.com/jvikstedt/bluestorm/network/json"
 )
 
 func onConnect(a *network.Agent) {
-	log.Printf("Agent connectedted %s\n", a.ID)
+	log.Printf("Agent connectedted %s\n", a.ID())
+	defaultRoom, _ := hub.DefaultManager().GetRoom(hub.DefaultRoomID)
+	hub.DefaultManager().UserToRoom(hub.UserID(a.ID()), hub.DefaultRoomID)
+
+	log.Printf("room has users: %v\n", defaultRoom.GetUsers())
 }
 
 func onDisconnect(a *network.Agent) {
-	log.Printf("Agent disconnected %s\n", a.ID)
+	log.Printf("Agent disconnected %s\n", a.ID())
+	defaultRoom, _ := hub.DefaultManager().GetRoom(hub.DefaultRoomID)
+	hub.DefaultManager().RemoveUser(hub.UserID(a.ID()))
+	log.Printf("room has users: %v\n", defaultRoom.GetUsers())
 }
 
 func main() {
